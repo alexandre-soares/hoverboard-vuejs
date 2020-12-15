b7dd8924077c66676c6042842513e2094491ce9b
Alexandre Soares <alexandresoares@Alexandres-MacBook-Pro.local>
2020-12-15 13:59:06 +0200

    initial commit, adding squares, colors, mouseenter and mouseleave

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Home />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Home
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: hidden;
+  background: #111;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container" id="container">
+    <div
+      class="square"
+      v-for="i in squares"
+      :key="i.index"
+      @mouseenter="setColor"
+      @mouseout="removeColor"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      colors: ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"],
+      squares: 810
+    };
+  },
+  mounted() {
+    this.createGrid();
+  },
+  methods: {
+    createGrid() {
+      for (let i = 0; i < this.squares; i++) {
+        const container = document.getElementById("container");
+        const square = document.createElement("div");
+
+        square.classList.add("square");
+
+        // square.addEventListener('mouseover', () => setColor(square))
+        // square.addEventListener('mouseout', () => removeColor(square))
+
+        container.appendChild(square);
+      }
+    },
+    setColor(event) {
+      const element = event.target;
+      const color = this.getRandomColor();
+      element.style.background = color;
+      element.style.boxShadow = `0 0 2px ${color}, 0 0 1rem ${color}`;
+    },
+    removeColor(event) {
+      const element = event.target;
+      element.style.background = "#474747";
+      element.style.boxShadow = "0 0 2px #000";
+    },
+    getRandomColor() {
+      return this.colors[Math.floor(Math.random() * this.colors.length)];
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 60rem;
+}
+
+.square {
+  background-color: #474747;
+  box-shadow: 0 0 2px #000;
+  height: 1.6rem;
+  width: 1.6rem;
+  margin: 2px;
+  transition: 2s ease;
+}
+
+.square:hover {
+  transition-duration: 0s;
+}
+</style>
